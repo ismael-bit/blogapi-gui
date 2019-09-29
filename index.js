c95511db68cd0f2da536ff630143ae449117e51d
Ismael Leon <paelosoft@gmail.com>
2019-09-28 21:40:00 -0400

    agregando login

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
+/*
 function saludar(){
 var nombre =document.getElementById("nombre_persona").value;
-alert("Hola "+ nombre + "!");
-};
+//alert("Hola "+ nombre + "!");
+document.getElementById("placeholder").textContent = "Hola " + nombre;
+}
+*/
+
+function login(){
+    var username =document.getElementById("username").value;
+    var password =document.getElementById("password").value;
+    var data ={
+        username: username,
+        password:password,
+        email:username
+    };
+    console.log(data);
+    
+    fetch(`${API_PATH}/login`, {
+        method: 'POST',
+        body: JSON.stringify(data), // data can be `string` or {object}!
+        headers:{
+          'Content-Type': 'application/json'
+        }
+      }).then(res => res.json())
+      .catch(error => console.error('Error:', error))
+      .then(response => console.log('Success:', response));
+}
+
+window.onload = function(){
+    document.getElementById("btnLogin").addEventListener('click',function(){login();});
+}
+    
+/*
+window.onload = function(){
+//document.getElementById("btnSaludar").addEventListener('click',function(){alert("Hola Persona");)});
+document.getElementById("btnSaludar").addEventListener('click',function(){saludar();});
+}
+*/
